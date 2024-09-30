f6acbbf037eb4ca0aa6ab73688a8df5a69fddab1
Supersllol <simon_lacroix@outlook.com>
2024-09-29 21:26:52 -0400

    test manettes

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,18 @@
 
 void setup() {
   CrcLib::Initialize();
+  Serial.begin(2000000);
 }
 
 void loop() {
   CrcLib::Update();
+  Serial.println();
+  if (CrcLib::IsCommValid())
+  {
+    CrcLib::PrintControllerState();
+  }
+  else
+  {
+    Serial.print("No remote controller successfully communicates with the CrcDuino");
+  }
 }
